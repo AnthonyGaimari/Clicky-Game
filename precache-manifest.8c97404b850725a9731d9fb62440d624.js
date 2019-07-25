c5d4f2dd910a72096c66e8d2aec74a377686d801
Anthony Gaimari <tgaimari@comcast.net>
2019-07-25 18:41:30 -0500

    Updates

---
+++ precache-manifest.8c97404b850725a9731d9fb62440d624.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb8384c61b24f054c7c03c77c3cc083",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "497276a1a4be246d8daf",
+    "url": "/Clicky-Game/static/css/main.20da250a.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/Clicky-Game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "497276a1a4be246d8daf",
+    "url": "/Clicky-Game/static/js/main.93501015.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "274682ee743b9a914e3c4d3b8cfe8814",
+    "url": "/Clicky-Game/static/media/an7.274682ee.jpg"
+  },
+  {
+    "revision": "81f73f8e23e344f41223a5f73451b38c",
+    "url": "/Clicky-Game/static/media/bs6.81f73f8e.jpg"
+  },
+  {
+    "revision": "31936e67abfa4e3f12a6acc9487ae82a",
+    "url": "/Clicky-Game/static/media/ck4.31936e67.jpg"
+  },
+  {
+    "revision": "96bd2f72e19e764ed88f5034389339b0",
+    "url": "/Clicky-Game/static/media/ck9.96bd2f72.jpg"
+  },
+  {
+    "revision": "00979288a1126abeef96915906c05902",
+    "url": "/Clicky-Game/static/media/cp17.00979288.jpg"
+  },
+  {
+    "revision": "342b9b125208eaa0dea8c471d79249cc",
+    "url": "/Clicky-Game/static/media/cs2.342b9b12.jpg"
+  },
+  {
+    "revision": "c551144b370bdc8ea717ba0710a64f69",
+    "url": "/Clicky-Game/static/media/gc8.c551144b.jpg"
+  },
+  {
+    "revision": "cbc35f36d4c96cb8e02c8f92ef642e02",
+    "url": "/Clicky-Game/static/media/jd5.cbc35f36.jpg"
+  },
+  {
+    "revision": "92887293cbae80380a0acf812df3bec1",
+    "url": "/Clicky-Game/static/media/jl14.92887293.jpg"
+  },
+  {
+    "revision": "678b7c2e05630f7b8a68775dea85f878",
+    "url": "/Clicky-Game/static/media/jt12.678b7c2e.jpg"
+  },
+  {
+    "revision": "5afbcbdec41addb96efb30c839c3cf3d",
+    "url": "/Clicky-Game/static/media/jv3.5afbcbde.jpg"
+  },
+  {
+    "revision": "b48766b845e361dde811753e012c6675",
+    "url": "/Clicky-Game/static/media/mc13.b48766b8.jpg"
+  },
+  {
+    "revision": "18931a1e5b3c7227a12522dc61e5716f",
+    "url": "/Clicky-Game/static/media/mk15.18931a1e.jpg"
+  },
+  {
+    "revision": "68e93e6a0351c5a9770b3ee0db6d41d8",
+    "url": "/Clicky-Game/static/media/ms1.68e93e6a.jpg"
+  },
+  {
+    "revision": "74bc6068aaa60ff446039e18110e6638",
+    "url": "/Clicky-Game/static/media/rr18.74bc6068.jpg"
+  },
+  {
+    "revision": "90ef62872525c09aec054c50f7214fb8",
+    "url": "/Clicky-Game/static/media/tb10.90ef6287.jpg"
+  },
+  {
+    "revision": "f4f7b22095f1eab6830f2fa8702d22d6",
+    "url": "/Clicky-Game/static/media/wh11.f4f7b220.jpg"
+  },
+  {
+    "revision": "ae1125bc330a2d9c35a175badfed483b",
+    "url": "/Clicky-Game/static/media/yd16.ae1125bc.jpg"
+  }
+]);
